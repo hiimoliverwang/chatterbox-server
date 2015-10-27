241d8b1cdd8f7c9585130927ee4369f431f0a2c2
hiimoliverwang <oliverwangmusic@gmail.com>
2015-10-26 19:18:35 -0700

    refactored to only call rewrite server once

--- server/request-handler.js
+++ server/request-handler.js
@@ -32,93 +32,24 @@ module.exports.requestHandler = function(request, response) {
   var headers = defaultCorsHeaders;
   headers['Content-Type'] = "application/json";
   console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode=404;
 
+    if (request.url in allowedUrl && request.method === 'GET'){
+      statusCode = 200;
+    } else if (request.method ==='POST' ){
+      statusCode = 201;
 
-      // console.log(request);
-      // console.log(storage)
-      // var test = '';
-      // if (request.url ==='/classes/messages'){
-
-
-
-      if(request.method ==='GET'){
-        if (!(request.url in allowedUrl)){
-          
-          response.writeHead(404, headers);
-          response.end();
-        } else {
-          response.writeHead(200,headers);
-          response.end(JSON.stringify(storage));
-        }
-      } else if (request.method ==='POST'){
-
-        request.on('data', function(d){
-          var data = '';
-          data+=d;
-
-          request.on('end',function(d){
-            console.log(data);
-            response.writeHead(201, headers);
-            storage.results.push(JSON.parse(data));
-            console.log(storage);
-            response.end();
-          });
+      request.on('data', function(d){
+        var data = '';
+        data+=d;
+        request.on('end',function(d){
+          storage.results.push(JSON.parse(data));
         });
-        // console.log(storage);
-        // storage.results.push(test);
-        // response.end(test);
-        }
-
-      
-    // var responseText = '';
-
-    //response.on('data', function(datum) {
-      //responseText += datum;
-    // })
-
-    // var body = JSON.parse(responseText);
-
-  // The outgoing status.
-
-  // See the note below about CORS headers.
-
-  //var statusCode = 200;
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // response.writeHead(statusCode, headers);
-
-
-
-
-
-    // response.end(JSON.stringify(request._postData));
-    
-  
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // response.end('hello world');
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
+      });
+    }
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(storage));
+}
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
